39bd572cbc351d0f66e07c9f2148c34586942abb
AttiyaDiantiFadli <attyadiantifadli@gmail.com>
2024-08-18 23:47:11 +0700

    Memperbaharui Configurasi Database

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'gaji',
+	'hostname' => 'srv1410.hstgr.io',
+	'username' => 'u740202808_Root_Attiya',
+	'password' => '!@34ATTiya',
+	'database' => 'u740202808_Attiya_Website',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
